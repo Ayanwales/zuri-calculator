1663556381a91defc4f14908bdf646e9f7172e2f
Ayanwales <Ayanwalesulaymon@gmail.com>
2022-06-22 18:11:08 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const num1 = parseInt(prompt("Enter First Number:"));
+const num2 = parseInt(prompt("Enter Second Number:"));
+
+let addition = num1 + num2;
+console.log(addition);
+
+let subtraction = num1 - num2;
+console.log(subtraction);
+
+
+let multiplication = num1 * num2;
+console.log(multiplication);
+
+
+let division = num1 / num2;
+console.log(division);
+alert('The addition of num1 and num2 is equal to' + ' ' + addition + '\n'
++ 'The subtraction of num2 from num1 is equal to' + ' ' + subtraction + '\n'
++ 'The multiplication of num1 and num2 is equal to' + ' ' + multiplication + '\n'
++ 'The division of num1 by num2 is equal to' + ' ' + division)
+
